493f88df4d78a1f49c816121ef30443124cf8c8e
Nguyen Huy <72247860+hyquaq@users.noreply.github.com>
2021-10-22 16:58:36 +0700

    change projects

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './app.scss';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Projects from './components/Projects';
-import Footer from './components/Footer';
-import Contacts from './components/Contacts';
+import logo from "./logo.svg";
+import "./app.scss";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
+import Contacts from "./components/Contacts";
+
+const cards = [
+  {
+    name: "quiz",
+    srcImg: "./imgs/quiz.png",
+    url: "https://hyquaq.github.io/WebQuestion/",
+  },
+  {
+    name: "count down",
+    srcImg: "./imgs/countdown.png",
+    url: "https://hyquaq.github.io/25m/",
+  },
+  {
+    name: "count day",
+    srcImg: "./imgs/countday.png",
+    url: "https://hyquaq.github.io/count-day/",
+  },
+  {
+    name: "Wallpaper Collection",
+    srcImg: "./imgs/desdev1.png",
+    url: "https://hyquaq.github.io/desdev-ex1/",
+  },
+  {
+    name: "Wallpaper Collection",
+    srcImg: "./imgs/desdev1.png",
+    url: "https://hyquaq.github.io/desdev-ex2/",
+  },
+  {
+    name: "Wallpaper Collection",
+    srcImg: "./imgs/desdev1.png",
+    url: "https://hyquaq.github.io/desdev-ex3/",
+  },
+  {
+    name: "Wallpaper Collection",
+    srcImg: "./imgs/desdev1.png",
+    url: "https://hyquaq.github.io/desdev-ex4/",
+  },
+  {
+    name: "Wallpaper Collection",
+    srcImg: "./imgs/desdev1.png",
+    url: "https://hyquaq.github.io/desdev-ex5/",
+  },
+  {
+    name: "Wallpaper Collection",
+    srcImg: "./imgs/desdev1.png",
+    url: "https://hyquaq.github.io/desdev-ex7/",
+  },
+  {
+    name: "Wallpaper Collection",
+    srcImg: "./imgs/desdev1.png",
+    url: "https://hyquaq.github.io/desdev-ex9/",
+  },
+];
 
 function App() {
+  console.log(cards);
+
   return (
-    <div className="app">
-      <h1>
-        Welcome to
-      </h1>
-
-      <img src={"./imgs/icon/facebook.png"} />
-
+    <div>
       <Header />
       <main>
         <Hero />
-        <Projects />
+        <Projects projects={cards} />
         <Contacts />
       </main>
       <Footer />
---
+++ src/components/Card.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+Card.propTypes = {
+    url: PropTypes.string,
+    srcImg: PropTypes.string,
+    name: PropTypes.string,
+}
+
+Card.defaultProps = {
+    url: '',
+    srcImg: '',
+    name: ''
+}
+
+function Card(props) {
+    const { url, name, srcImg } = { ...props };
+    return (
+        <a href={url} className="project">
+            <img src={srcImg} alt="quiz app" />
+            <p className="name">
+                {name}
+            </p>
+        </a>
+    )
+}
+
+
+
+export default Card
+
