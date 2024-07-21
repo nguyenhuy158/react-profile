3e4b8010888440f91d3532a39836f4622862b542
nguyenhuy158 <huy.ntq02@gmail.com>
2024-07-21 09:39:43 +0700

    fix some error

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,13 @@ echo "TIMESTAMP: $TIMESTAMP"
 
 docker build --platform linux/amd64 \
     -t registry.gitlab.com/huy.ntq02/farmnet/profile:latest-amd64v8 \
-    -t registry.gitlab.com/huy.ntq02/farmnet/profile:$TIMESTAMP-amd64v8 \   
-    .
+    -t registry.gitlab.com/huy.ntq02/farmnet/profile:$TIMESTAMP-amd64v8 .
+
+docker build --platform linux/arm64 \
+    -t registry.gitlab.com/huy.ntq02/farmnet/profile:latest-arm64v8 \
+    -t registry.gitlab.com/huy.ntq02/farmnet/profile:$TIMESTAMP-arm64v8 .
 
 docker push registry.gitlab.com/huy.ntq02/farmnet/profile:latest-amd64v8
+docker push registry.gitlab.com/huy.ntq02/farmnet/profile:latest-arm64v8
 docker push registry.gitlab.com/huy.ntq02/farmnet/profile:$TIMESTAMP-amd64v8
+docker push registry.gitlab.com/huy.ntq02/farmnet/profile:$TIMESTAMP-arm64v8
